4dbb553f1b409644dd311716544a59291bbcf978
Peter Turner <git-commit@flintsteel7.com>
2017-02-11 19:09:35 -0800

    change min height to height variation, display slider values to user

--- js/script.js
+++ js/script.js
@@ -3,33 +3,39 @@ var ctx = canvas.getContext('2d');
 
 var grass = {
   "max_height": 40,
-  "min_height": 7,
+  "height_var": 33,
   "angle": -5,
   "colors": ['#1f7a1f', '#29a329', '#33cc33', '#5cd65c', '#85e085']
 };
 
-var grassMinHeightSlider = document.getElementById('grassMinHeight');
-  grassMinHeightSlider.min = 1;
-  grassMinHeightSlider.max = 250;
-  grassMinHeightSlider.defaultValue = 7;
-var grassMaxHeightSlider = document.getElementById('grassMaxHeight');
-  grassMaxHeightSlider.min = 1;
-  grassMaxHeightSlider.max = 250;
-  grassMaxHeightSlider.defaultValue = 40;
-var grassAngleSlider = document.getElementById('grassAngle');
-  grassAngleSlider.min = 0;
-  grassAngleSlider.max = 25;
-  grassAngleSlider.defaultValue = 5;
-var resetButton = document.getElementById('formReset');
-
-grassMinHeightSlider.addEventListener('mouseup', setGrassMinHeight);
-grassMaxHeightSlider.addEventListener('mouseup', setGrassMaxHeight);
-grassAngleSlider.addEventListener('mouseup', setGrassAngle);
-resetButton.addEventListener('onclick', drawGrass);
-
 // Calculate canvas size
 canvas.width = document.documentElement.clientWidth * 0.79;
 canvas.height = document.documentElement.clientHeight;
+
+var grassMaxHeightSlider = document.getElementById('grassMaxHeight');
+var grassMaxHeightDisp = document.getElementById('grassMaxHeightValue');
+  grassMaxHeightSlider.min = 1;
+  grassMaxHeightSlider.max = canvas.height;
+  grassMaxHeightSlider.defaultValue = 40;
+  grassMaxHeightDisp.innerHTML = grassMaxHeightSlider.value;
+var grassHeightVarSlider = document.getElementById('grassHeightVar');
+var grassHeightVarDisp = document.getElementById('grassHeightVarValue');
+  grassHeightVarSlider.min = 0;
+  grassHeightVarSlider.max = canvas.height;
+  grassHeightVarSlider.defaultValue = 33;
+  grassHeightVarDisp.innerHTML = grassHeightVarSlider.value;
+var grassAngleSlider = document.getElementById('grassAngle');
+var grassAngleDisp = document.getElementById('grassAngleValue');
+  grassAngleSlider.min = 0;
+  grassAngleSlider.max = 45;
+  grassAngleSlider.defaultValue = 5;
+  grassAngleDisp.innerHTML = grassAngleSlider.value;
+var resetButton = document.getElementById('formReset');
+
+grassMaxHeightSlider.addEventListener('mouseup', setGrassMaxHeight);
+grassHeightVarSlider.addEventListener('mouseup', setGrassHeightVar);
+grassAngleSlider.addEventListener('mouseup', setGrassAngle);
+resetButton.addEventListener('onclick', drawGrass);
 
 // Draw Grass
 drawGrass();
@@ -39,21 +45,21 @@ function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function setGrassMinHeight() {
-  grass.min_height = parseInt(grassMinHeightSlider.value);
-  // console.log("Min Height = " + grassMinHeightSlider.value);
+function setGrassMaxHeight() {
+  grass.max_height = parseInt(grassMaxHeightSlider.value);
+  grassMaxHeightDisp.innerHTML = grassMaxHeightSlider.value;
   drawGrass();
 }
 
-function setGrassMaxHeight() {
-  grass.max_height = parseInt(grassMaxHeightSlider.value);
-  // console.log("Max Height = " + grassMaxHeightSlider.value);
+function setGrassHeightVar() {
+  grass.height_var = parseInt(grassHeightVarSlider.value);
+  grassHeightVarDisp.innerHTML = grassHeightVarSlider.value;
   drawGrass();
 }
 
 function setGrassAngle() {
   grass.angle = parseInt(grassAngleSlider.value);
-  // console.log("Angle = " + grassAngleSlider.value);
+  grassAngleDisp.innerHTML = grassAngleSlider.value;
   drawGrass()
 }
 
@@ -62,7 +68,7 @@ function drawGrass() {
   for (var i = 0; i < canvas.width; i++) {
     ctx.beginPath();
     ctx.moveTo(i,canvas.height);
-    ctx.lineTo(getRandInt(i - grass.angle, i + grass.angle), getRandInt(canvas.height - grass.max_height, canvas.height - grass.min_height));
+    ctx.lineTo(getRandInt(i - grass.angle, i + grass.angle), getRandInt(canvas.height - grass.max_height, canvas.height - (grass.max_height - grass.height_var)));
     ctx.strokeStyle = grass.colors[getRandInt(0, grass.colors.length - 1)];
     ctx.stroke();
     ctx.closePath();
