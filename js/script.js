da87b889453121fb9ef44c3fe809f4b42987ca83
Peter Turner <git-commit@flintsteel7.com>
2017-10-21 22:44:33 -0700

    draw fractal branch lines

--- js/script.js
+++ js/script.js
@@ -16,22 +16,22 @@ canvas.height = document.documentElement.clientHeight;
 
 const grassMaxHeightSlider = document.getElementById('grassMaxHeight');
 const grassMaxHeightDisp = document.getElementById('grassMaxHeightValue');
-  grassMaxHeightSlider.min = 1;
-  grassMaxHeightSlider.max = canvas.height;
-  grassMaxHeightSlider.defaultValue = 40;
-  grassMaxHeightDisp.innerHTML = grassMaxHeightSlider.value;
+grassMaxHeightSlider.min = 1;
+grassMaxHeightSlider.max = canvas.height;
+grassMaxHeightSlider.defaultValue = 40;
+grassMaxHeightDisp.innerHTML = grassMaxHeightSlider.value;
 const grassHeightVarSlider = document.getElementById('grassHeightVar');
 const grassHeightVarDisp = document.getElementById('grassHeightVarValue');
-  grassHeightVarSlider.min = 0;
-  grassHeightVarSlider.max = 40;
-  grassHeightVarSlider.defaultValue = 33;
-  grassHeightVarDisp.innerHTML = grassHeightVarSlider.value;
+grassHeightVarSlider.min = 0;
+grassHeightVarSlider.max = 40;
+grassHeightVarSlider.defaultValue = 33;
+grassHeightVarDisp.innerHTML = grassHeightVarSlider.value;
 const grassAngleSlider = document.getElementById('grassAngle');
 const grassAngleDisp = document.getElementById('grassAngleValue');
-  grassAngleSlider.min = 0;
-  grassAngleSlider.max = 45;
-  grassAngleSlider.defaultValue = 5;
-  grassAngleDisp.innerHTML = grassAngleSlider.value;
+grassAngleSlider.min = 0;
+grassAngleSlider.max = 45;
+grassAngleSlider.defaultValue = 5;
+grassAngleDisp.innerHTML = grassAngleSlider.value;
 const resetButton = document.getElementById('formReset');
 
 grassMaxHeightSlider.addEventListener('mouseup', setGrassMaxHeight);
@@ -74,7 +74,7 @@ function drawGrass() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < canvas.width; i++) {
     ctx.beginPath();
-    ctx.moveTo(i,canvas.height);
+    ctx.moveTo(i, canvas.height);
     ctx.lineTo(getRandInt(i - grass.angle, i + grass.angle), getRandInt(canvas.height - grass.max_height, canvas.height - (grass.max_height - grass.height_var)));
     ctx.strokeStyle = grass.colors[getRandInt(0, grass.colors.length - 1)];
     ctx.stroke();
@@ -82,22 +82,50 @@ function drawGrass() {
   }
 }
 
-let center = canvas.width / 2;
-
-function drawCone (center, width, height, taper) {
-  ctx.moveTo(center - (width / 2), canvas.height);
-  ctx.lineTo(center - (taper / 2), canvas.height - height);
-  ctx.lineTo(center + (taper / 2), canvas.height - height);
-  ctx.lineTo(center + (width / 2), canvas.height);
+function drawTrunk(height, base_width, end_width, branch_lev, position = (canvas.width / 2)) {
+  ctx.beginPath();
+  ctx.moveTo(position - (base_width / 2), canvas.height);
+  ctx.lineTo(position - (end_width / 2), canvas.height - height);
+  ctx.lineTo(position + (end_width / 2), canvas.height - height);
+  ctx.lineTo(position + (base_width / 2), canvas.height);
+  ctx.fillStyle = '#3B2112';
   ctx.fill();
   ctx.closePath();
+  drawBranch({
+    x: position,
+    y: canvas.height - height
+  }, height / 2, end_width, ((end_width * end_width) / base_width), 45, branch_lev);
 }
 
-function newDrawCone (start_coords, width, length, taper, angle) {
-  ctx.moveTo(start_coords.x - (width / 2), start_coords.y);
+function drawBranch(start, length, base_width, end_width, angle, branch_lev) {
+  if (branch_lev > 0) {
+    const x_dif = length * Math.cos(angle / 180 * Math.PI);
+    const y_dif = length * Math.sin(angle / 180 * Math.PI);
+    ctx.beginPath();
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(start.x + x_dif, start.y - y_dif);
+    ctx.strokeStyle = '#3B2112';
+    ctx.stroke();
+    ctx.closePath();
+    if ((branch_lev - 1) > 0) {
+      drawBranch({
+        x: start.x + x_dif,
+        y: start.y - y_dif
+      }, length / 2, end_width, ((end_width * end_width) / base_width), angle, branch_lev - 1);
+    }
+    ctx.beginPath();
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(start.x - x_dif, start.y - y_dif);
+    ctx.strokeStyle = '#3B2112';
+    ctx.stroke();
+    if ((branch_lev - 1) > 0) {
+      drawBranch({
+        x: start.x - x_dif,
+        y: start.y - y_dif
+      }, length / 2, end_width, ((end_width * end_width) / base_width), angle, branch_lev - 1);
+    }
+
+  }
 }
 
-drawCone(canvas.width / 2, 40, 500, 25);
-
-newDrawCone({x: canvas.width / 2, y: canvas.height}, 40, 500, 25, 90);
-
+drawTrunk(350, 40, 25, 7);
