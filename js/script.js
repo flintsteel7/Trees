485791ec4a0c298e52d6b4c0ad6343affdee7095
Peter Turner <git-commit@flintsteel7.com>
2017-11-03 23:04:43 -0700

    add thickness to branches

--- js/script.js
+++ js/script.js
@@ -103,23 +103,28 @@ function drawTrunk(height, base_width, end_width, branch_lev, position = (canvas
 
 function drawBranch(start, length, base_width, end_width, angle, branch_lev) {
   if (branch_lev > 0) {
-    const x_dif = length * Math.cos(angle);
-    const y_dif = length * Math.sin(angle);
+    const end = {
+      x: start.x + (length * Math.cos(angle)),
+      y: start.y - (length * Math.sin(angle))
+    }
+    const half_base = (base_width / 2);
+    const half_end = (end_width / 2);
+    const angle90 = (Math.PI / 2);
     c.beginPath();
-    c.moveTo(start.x, start.y);
-    c.lineTo(start.x + x_dif, start.y - y_dif);
-    c.strokeStyle = '#3B2112';
-    c.stroke();
+    c.moveTo(start.x + (half_base * Math.cos(angle + angle90)),
+      start.y - (half_base * Math.sin(angle + angle90)));
+    c.lineTo(end.x + (half_end * Math.cos(angle + angle90)),
+      end.y - (half_end * Math.sin(angle + angle90)));
+    c.lineTo(end.x + (half_end * Math.cos(angle - angle90)),
+      end.y - (half_end * Math.sin(angle - angle90)));
+    c.lineTo(start.x + (half_base * Math.cos(angle - angle90)),
+      start.y - (half_base * Math.sin(angle - angle90)));
+    c.fillStyle = '#3B2112';
+    c.fill();
     c.closePath();
     if ((branch_lev - 1) > 0) {
-      drawBranch({
-        x: start.x + x_dif,
-        y: start.y - y_dif
-      }, length * 0.6, end_width, ((end_width * end_width) / base_width), angle - (Math.PI / 4), branch_lev - 1);
-      drawBranch({
-        x: start.x + x_dif,
-        y: start.y - y_dif
-      }, length * 0.6, end_width, ((end_width * end_width) / base_width), angle + (Math.PI / 4), branch_lev - 1);
+      drawBranch(end, length * 0.6, end_width, ((end_width * end_width) / base_width), angle - (Math.PI / 4), branch_lev - 1);
+      drawBranch(end, length * 0.6, end_width, ((end_width * end_width) / base_width), angle + (Math.PI / 4), branch_lev - 1);
     }
     /*
     c.beginPath();
@@ -138,4 +143,4 @@ function drawBranch(start, length, base_width, end_width, angle, branch_lev) {
   }
 }
 
-drawTrunk(300, 40, 25, 7);
+drawTrunk(300, 40, 25, 13);
