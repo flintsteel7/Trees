b0f0edaa8875ddc631b8e9a0ded07aad38269d93
Peter Turner <git-commit@flintsteel7.com>
2017-11-30 20:45:43 -0800

    add controls for tree trunk height, width, and taper

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@
 const canvas = document.getElementById('mainCanvas');
 const c = canvas.getContext('2d');
 
+let tree = {
+  "height": 285,
+  "width": 40,
+  "taper": 15,
+  "branch_lev": 13
+}
+
 let grass = {
   "max_height": 40,
   "height_var": 33,
@@ -14,6 +21,33 @@ let grass = {
 canvas.width = window.innerWidth * 0.79;
 canvas.height = window.innerHeight;
 
+// Tree controls
+const treeTrunkHeightSlider = document.getElementById('treeTrunkHeight');
+const treeTrunkHeightDisp = document.getElementById('treeTrunkHeightValue');
+treeTrunkHeightSlider.min = 1;
+treeTrunkHeightSlider.max = canvas.height;
+treeTrunkHeightSlider.defaultValue = Math.round(canvas.height * 0.4);
+treeTrunkHeightDisp.innerHTML = treeTrunkHeightSlider.value;
+const treeTrunkWidthSlider = document.getElementById('treeTrunkWidth');
+const treeTrunkWidthDisp = document.getElementById('treeTrunkWidthValue');
+treeTrunkWidthSlider.min = 1;
+treeTrunkWidthSlider.max = 150;
+treeTrunkWidthSlider.defaultValue = 40;
+treeTrunkWidthDisp.innerHTML = treeTrunkWidthSlider.value;
+const treeTaperSlider = document.getElementById('treeTaper');
+const treeTaperDisp = document.getElementById('treeTaperValue');
+treeTaperSlider.min = 1;
+treeTaperSlider.max = tree.width - 1;
+treeTaperSlider.defaultValue = 15;
+treeTaperDisp.innerHTML = treeTaperSlider.value;
+
+// Tree listeners
+treeTrunkHeightSlider.addEventListener('mouseup', setTreeTrunkHeight);
+treeTrunkWidthSlider.addEventListener('mouseup', setTreeTrunkWidth);
+treeTaperSlider.addEventListener('mouseup', setTreeTaper);
+
+
+// Grass controls
 const grassMaxHeightSlider = document.getElementById('grassMaxHeight');
 const grassMaxHeightDisp = document.getElementById('grassMaxHeightValue');
 grassMaxHeightSlider.min = 1;
@@ -34,17 +68,41 @@ grassAngleSlider.defaultValue = 5;
 grassAngleDisp.innerHTML = grassAngleSlider.value;
 const resetButton = document.getElementById('formReset');
 
+// Grass listeners
 grassMaxHeightSlider.addEventListener('mouseup', setGrassMaxHeight);
 grassHeightVarSlider.addEventListener('mouseup', setGrassHeightVar);
 grassAngleSlider.addEventListener('mouseup', setGrassAngle);
-resetButton.addEventListener('onclick', drawGrass);
-
-// Draw Grass
-drawGrass();
+resetButton.addEventListener('onclick', drawScene(tree, grass));
 
 // Functions
-function getRandInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function drawScene(tree, grass) {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  drawTree(tree);
+  drawGrass(grass);
+}
+
+function setTreeTrunkHeight() {
+  tree.height = parseInt(treeTrunkHeightSlider.value);
+  treeTrunkHeightDisp.innerHTML = treeTrunkHeightSlider.value;
+  drawScene(tree, grass);
+}
+
+function setTreeTrunkWidth() {
+  // TODO update max taper based on width
+  tree.width = parseInt(treeTrunkWidthSlider.value);
+  treeTrunkWidthDisp.innerHTML = treeTrunkWidthSlider.value;
+  drawScene(tree, grass);
+}
+
+function setTreeTaper() {
+  tree.taper = parseInt(treeTaperSlider.value);
+  if (tree.taper > tree.width - 1) {
+    tree.taper = tree.width - 1;
+    treeTaperSlider.max = tree.taper;
+    treeTaperSlider.value = tree.taper;
+  }
+  treeTaperDisp.innerHTML = treeTaperSlider.value;
+  drawScene(tree, grass);
 }
 
 function setGrassMaxHeight() {
@@ -55,31 +113,38 @@ function setGrassMaxHeight() {
     setGrassHeightVar();
   }
   grassHeightVarSlider.max = grassMaxHeightSlider.value;
-  drawGrass();
+  drawScene(tree, grass);
 }
 
 function setGrassHeightVar() {
   grass.height_var = parseInt(grassHeightVarSlider.value);
   grassHeightVarDisp.innerHTML = grassHeightVarSlider.value;
-  drawGrass();
+  drawScene(tree, grass);
 }
 
 function setGrassAngle() {
   grass.angle = parseInt(grassAngleSlider.value);
   grassAngleDisp.innerHTML = grassAngleSlider.value;
-  drawGrass()
+  drawScene(tree, grass)
 }
 
-function drawGrass() {
-  c.clearRect(0, 0, canvas.width, canvas.height);
+function drawGrass(grass_params) {
   for (let i = 0; i < canvas.width; i++) {
     c.beginPath();
     c.moveTo(i, canvas.height);
-    c.lineTo(getRandInt(i - grass.angle, i + grass.angle), getRandInt(canvas.height - grass.max_height, canvas.height - (grass.max_height - grass.height_var)));
-    c.strokeStyle = grass.colors[getRandInt(0, grass.colors.length - 1)];
+    c.lineTo(getRandInt(i - grass_params.angle, i + grass_params.angle), getRandInt(canvas.height - grass_params.max_height, canvas.height - (grass_params.max_height - grass_params.height_var)));
+    c.strokeStyle = grass_params.colors[getRandInt(0, grass_params.colors.length - 1)];
     c.stroke();
     c.closePath();
   }
+}
+
+function getRandInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function drawTree(tree_params) {
+  drawTrunk(tree_params.height, tree_params.width, tree_params.width - tree_params.taper, tree_params.branch_lev);
 }
 
 function drawTrunk(height, base_width, end_width, branch_lev, position = (canvas.width / 2)) {
@@ -106,7 +171,7 @@ function drawBranch(start, length, base_width, end_width, angle, branch_lev) {
     const end = {
       x: start.x + (length * Math.cos(angle)),
       y: start.y - (length * Math.sin(angle))
-    }
+    };
     const half_base = (base_width / 2);
     const half_end = (end_width / 2);
     const angle90 = (Math.PI / 2);
@@ -126,21 +191,5 @@ function drawBranch(start, length, base_width, end_width, angle, branch_lev) {
       drawBranch(end, length * 0.6, end_width, ((end_width * end_width) / base_width), angle - (Math.PI / 4), branch_lev - 1);
       drawBranch(end, length * 0.6, end_width, ((end_width * end_width) / base_width), angle + (Math.PI / 4), branch_lev - 1);
     }
-    /*
-    c.beginPath();
-    c.moveTo(start.x, start.y);
-    c.lineTo(start.x - x_dif, start.y - y_dif);
-    c.strokeStyle = '#3B2112';
-    c.stroke();
-    if ((branch_lev - 1) > 0) {
-      drawBranch({
-        x: start.x - x_dif,
-        y: start.y - y_dif
-      }, length / 2, end_width, ((end_width * end_width) / base_width), (angle - angle), branch_lev - 1);
-    }
-    */
-
   }
 }
-
-drawTrunk(300, 40, 25, 13);
