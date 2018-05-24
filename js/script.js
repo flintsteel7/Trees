fd7ef048db15c154981ae12762169906f4724328
Peter Turner <git-commit@flintsteel7.com>
2018-05-23 23:15:43 -0700

    make progress branch calculation functions

--- js/script.js
+++ js/script.js
@@ -233,13 +233,18 @@ function calcLengthRandomness(variable) {
   return (variable * Math.random() * 3) * posOrNeg;
 }
 
+function calcRandomness(variable, factor) {
+  const posOrNeg = Math.round(Math.random()) * 2 - 1; // produces 1 or -1
+  return (variable * Math.random() * factor) * posOrNeg;
+}
+
 function calcTree(tree_params) {
   const this_tree = [];
   const trunk = calcTrunk({...tree_params});
   this_tree.push(trunk.coords);
   const branches = calcBranches({
     start: trunk.fork,
-    parent_angle: 90,
+    parent_angle: Math.PI / 2,
     ...tree_params,
   });
   return this_tree;
@@ -312,6 +317,34 @@ function drawTrunk({length, base_width, end_width, branch_lev, angle_var, length
   );
 }
 
+function calcBranches({start, width, taper, length, parent_angle, length_var, angle_var, branch_lev}) {
+  const branches = [];
+  const params = {
+    start,
+    base_width: width,
+    end_width: width - taper,
+  };
+  branches.push(calcBranch({
+    ...params,
+    length: length * 0.6 + calcRandomness(length_var, 3),
+    angle: parent_angle + (Math.PI / 4 + calcRandomness(angle_var, 0.05)),
+  }));
+  branches.push(calcBranch({
+    ...params,
+    length: length * 0.6 + calcRandomness(length_var, 3),
+    angle: parent_angle - (Math.PI / 4 + calcRandomness(angle_var, 0.05)),
+  }));
+}
+
+function calcBranch({start, base_width, end_width, length, angle}) {
+  const half_base = base_width / 2;
+  const half_end = end_width / 2;
+  const fork = {
+    x: start.x + (length * Math.cos(angle)),
+    y: start.y - (length * Math.sin(angle)),
+  };
+}
+
 function calcTrunk({position, width, length, taper}) {
   const half_base = width / 2;
   const half_end = (width - taper) / 2;
@@ -336,10 +369,6 @@ function calcTrunk({position, width, length, taper}) {
       },
     ],
   }
-}
-
-function calcBranches({start, width, length, parent_angle, length_var, angle_var, branch_lev}) {
-
 }
 
 function drawBranch(start, length, base_width, end_width, angle, branch_lev, angle_var, length_var) {
