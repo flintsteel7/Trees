25c78397a3d905d7b0abb67a5410c1938d8d702e
Peter Turner <git-commit@flintsteel7.com>
2018-08-02 08:52:55 -0700

    calculate and draw first two branches

--- src/lib/calculate-tree.js
+++ src/lib/calculate-tree.js
@@ -4,44 +4,64 @@ function calcTree(tree_params) {
   const this_tree = [];
   const trunk = calcTrunk({...tree_params});
   this_tree.push(trunk.coords);
-  const branches = calcBranches({
+  const branching_tree = this_tree.concat(calcBranches({
     start: trunk.fork,
     parent_angle: Math.PI / 2,
     ...tree_params,
-  });
-  console.log(branches);
-  return this_tree;
+  }));
+  console.log(branching_tree)
+  return branching_tree;
 }
 
 function calcBranches({start, width, taper, length, parent_angle, length_var, angle_var, branch_lev}) {
   const branches = [];
   const params = {
     start,
-    base_width: width,
-    end_width: width - taper,
+    base_width: width - taper,
+    end_width: width - (2 * taper),
   };
   branches.push(calcBranch({
     ...params,
     length: length * 0.6 + calcRandomness(length_var, 3),
     angle: parent_angle + (Math.PI / 4 + calcRandomness(angle_var, 0.05)),
-  }));
+  }).coords);
   branches.push(calcBranch({
     ...params,
     length: length * 0.6 + calcRandomness(length_var, 3),
     angle: parent_angle - (Math.PI / 4 + calcRandomness(angle_var, 0.05)),
-  }));
+  }).coords);
+  return branches;
 }
 
 function calcBranch({start, base_width, end_width, length, angle}) {
+  const end = {
+    x: start.x + (length * Math.cos(angle)),
+    y: start.y - (length * Math.sin(angle))
+  };
   const half_base = base_width / 2;
   const half_end = end_width / 2;
-  const fork = {
-    x: start.x + (length * Math.cos(angle)),
-    y: start.y - (length * Math.sin(angle)),
+  const angle90 = (Math.PI / 2);
+  return {
+    fork: end,
+    coords: [
+      {
+        x: start.x + (half_base * Math.cos(angle + angle90)),
+        y: start.y - (half_base * Math.sin(angle + angle90))
+      },
+      {
+        x: end.x + (half_end * Math.cos(angle + angle90)),
+        y: end.y - (half_end * Math.sin(angle + angle90))
+      },
+      {
+        x: end.x + (half_end * Math.cos(angle - angle90)),
+        y: end.y - (half_end * Math.sin(angle - angle90))
+      },
+      {
+        x: start.x + (half_base * Math.cos(angle - angle90)),
+        y: start.y - (half_base * Math.sin(angle - angle90))
+      }
+    ]
   };
-  // TODO calculate coordinates
-  const coords = [];
-  return {fork, coords}
 }
 
 function calcTrunk({position, width, length, taper}) {
